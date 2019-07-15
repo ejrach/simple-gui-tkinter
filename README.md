d4e98693626beed75b5bd7ddb4753b82c6cd44b6
ejrach <eric.rach@gmail.com>
2019-07-14 19:31:57 -0500

    Updated GUI to convert Kg to grams, pounds and ounces

---
+++ README.md
@@ -0,0 +1,18 @@
+# course-python-mega
+
+Application: Simple GUI built with Tkinter
+
+Python Framework: Tkinter
+
+
+Inputs: script1.py
+Outputs: GUI
+
+To run:
+1. At the terminal window, type: $python3 script1.py
+2. Enter a Kg value
+
+Description:
+This program will convert Kg to grams, pounds and ounces and display
+them at the bottom of the GUI window.
+
--- script1.py
+++ script1.py
@@ -1,9 +1,9 @@
 from tkinter import *
 
-
 # Basic description of the GUI control layout.
 # If this is the window, then controls are placed
-# in this grid
+# in this grid based off a row,column coordinate system.
+#
 #          column
 #       =====================================
 #  row  |     0,0   |    0,1    |    0,2    |
@@ -11,31 +11,44 @@ from tkinter import *
 #       |     1,0   |    1,1    |    1,2    |
 #       =====================================
 
-
-
 # create the active window
 window = Tk()
 
-def km_to_miles():
-    miles = float(e1_value.get())*1.6
-    t1.insert(END,miles)
+def kg_conversion():
+    grams = float(kg_e_value.get())*1000
+    pounds = float(kg_e_value.get())*2.20462
+    ounces = float(kg_e_value.get())*35.274
+    tbGrams.insert(END,grams)
+    tbPounds.insert(END, pounds)
+    tbOunces.insert(END, ounces)
+
+# create a label widget, and tell it "which container" to be displayed
+lblKg = Label(window, text="Kg")
+lblKg.grid(row=0, column=0)
+
+# entry widget for kg (takes user input), placed in the window container
+kg_e_value = StringVar()
+e1 = Entry(window, textvariable=kg_e_value)
+e1.grid(row=0, column=1)
 
 # create a button widget, and tell it "which container" to be displayed
 # https://www.tutorialspoint.com/python/tk_button
-b1 = Button(window, text = "Execute", command = km_to_miles)
+btnConvert = Button(window, text = "Convert", command = kg_conversion)
 # tell the button where to be displayed in the window
-b1.grid(row=0,column=0)
+btnConvert.grid(row=0,column=2)
 
-# create an entry widget (takes user input)
-e1_value = StringVar()
-e1 = Entry(window, textvariable=e1_value)
-e1.grid(row=0, column=1)
+# text widget for grams display, placed in the window container
+tbGrams = Text(window, height=1,width=10)
+tbGrams.grid(row=1,column=0)
 
-# create a text widget
-t1 = Text(window, height=1,width=20)
-t1.grid(row=0,column=2)
+# text widget for pounds display, placed in the window container
+tbPounds = Text(window, height=1,width=10)
+tbPounds.grid(row=1,column=1)
 
+# text widget for ounces display, placed in the window container
+tbOunces = Text(window, height=1,width=10)
+tbOunces.grid(row=1,column=2)
 
-# 
+# Execute the main loop of tkinter
 window.mainloop()
 
